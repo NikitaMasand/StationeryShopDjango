01171344418a0136e12ac82dbb8f7e36f50313cf
NikitaMasand <masandnikita@gmail.com>
2019-02-22 19:09:05 +0530

    Added model of product, views and templates and did styling

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse('hello world') #returned plain text to the browser..to let django know about this function, we add a
+    #returned plain text to the browser..to let django know about this function, we add a
     #path or mapping in urls list in urls.py
-
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products':products})
+                  #this {} contains the data to be passed to our html file
 
 def new(request):
     return HttpResponse('new product')
