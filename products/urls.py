61ce6f4c4fe7de6acb2997cca326ddb5a932f675
NikitaMasand <masandnikita@gmail.com>
2019-02-23 17:05:47 +0530

    add a new product and bit change in styling

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,9 @@ from . import views
 # '' means the root..i.e what comes as soon as you start the site. In our case it's products
 
 urlpatterns = [
-    path('', views.index), # we are not calling the index function, just passing the reference. django will take care of
+    path('', views.index, name = 'index'), # we are not calling the index function, just passing the reference. django will take care of
     #calling it when needed
-    path('new',views.new)
+    path('new',views.new),
+    path('add_new/',views.add_new, name = 'add_new'),
 ]
 # in this list we map various urls to the view functions
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
-
+from .forms import ProductForm
+from django.shortcuts import redirect
 
 def index(request):
     #returned plain text to the browser..to let django know about this function, we add a
@@ -12,3 +13,15 @@ def index(request):
 
 def new(request):
     return HttpResponse('new product')
+
+
+def add_new(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.save()
+            return redirect('index')
+    else:
+        form = ProductForm()
+    return render(request,'add_new.html',{'form':form})
